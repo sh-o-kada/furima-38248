388dc40b3eb54b1abfb41541cd4b5975e874a053
sh-o-kada <shokada.25y@gmail.com>
2022-08-09 22:08:04 +0900

    usersテーブルのバリデーション付与
    deviseの導入
    usersテーブルにバリデーションの付与

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "" } do
+    validates :encrypted_password
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角文字を使用してください" } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: "" } do
+    validates :last_name_spelling
+    validates :first_name_spelling
+  end
+  validates :birthday, presence: true
+end
