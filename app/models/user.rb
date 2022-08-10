a4f990fea0ea92a8021508eb9e5762ad4d4694ae
sh-o-kada <shokada.25y@gmail.com>
2022-08-10 15:50:53 +0900

    バリデーションの最終修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "には半角英数両方を含めて設定してください(全角不可)" } do
     validates :password
   end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一－-龥々ー]+\z/, message: "には全角文字を使用してください" } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー－]+\z/, message: "には全角文字を使用してください" } do
     validates :last_name
     validates :first_name
   end
